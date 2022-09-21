6d4022959be5d36669a1a21dd8ba9639a0d427d2
Pritam Soni <23050213+pritamsoni-hsr@users.noreply.github.com>
2022-09-21 18:44:21 +0530

    fix: #2 add additional http methods

--- lightning_api_access/ui/src/App.tsx
+++ lightning_api_access/ui/src/App.tsx
@@ -1,5 +1,4 @@
 import { CopyAllRounded } from "@mui/icons-material";
-import axios from "axios";
 import {
   Box,
   Container,
@@ -19,10 +18,11 @@ import { useClipboard } from "utils";
 const queryClient = new QueryClient();
 
 type APIEndpoint = Partial<{
+  name: string;
   url: string;
-  method: "GET";
+  method: "GET" | "POST" | "PUT";
   request: any;
-  response: object;
+  response: object | string;
   input_query: string;
 }>;
 
@@ -34,7 +34,36 @@ function Main() {
   React.useEffect(() => {
     const update = async () => {
       try {
-        const { data } = await axios.get<{ apis: APIEndpoint[] }>(`${window.location.origin}/api_metadata.json`);
+        // const { data } = await axios.get<{ apis: APIEndpoint[] }>(`${window.location.origin}/api_metadata.json`);
+        const data: { apis: APIEndpoint[] } = {
+          apis: [
+            {
+              name: "get image by id",
+              url: "/get_image",
+              method: "GET",
+              request: { id: "string" },
+              response:
+                '{\n  "id": "...",\n  "image": "...",\n  "status": "..."\n}',
+            },
+            {
+              name: "list images",
+              url: "/list_images",
+              method: "GET",
+              request: { size: "number" },
+              response: [
+                { image: "...", status: "..." },
+                { image: "...", status: "..." },
+              ],
+            },
+            {
+              name: "resize image",
+              url: "/resize",
+              method: "POST",
+              request: { size: "number" },
+              response: { image: "...", status: "..." },
+            },
+          ],
+        };
         setApiMetadata(data.apis);
       } catch (error) {
         enqueueSnackbar({
@@ -55,7 +84,14 @@ function Main() {
   }, [enqueueSnackbar]);
 
   return (
-    <Box display={"flex"} justifyContent={"center"} alignItems={"center"} height={"inherit"} flexDirection={"column"} gap={3}>
+    <Box
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      flexDirection={"column"}
+      gap={3}
+      paddingY={4}
+    >
       {apiMetadata.map((e) => (
         <RenderApiEndpoint {...e} key={e.url} />
       ))}
@@ -90,17 +126,25 @@ const RenderApiEndpoint = (props: APIEndpoint) => {
   return (
     <Container maxWidth={"md"}>
       <Stack minWidth={"300px"}>
-        <Typography>This is an API endpoint for ...</Typography>
+        <Typography>This is an API endpoint to {props.name}</Typography>
         <Box height={20} />
         <Box
           sx={(theme) => ({
             border: `1px solid ${theme.palette.primary.main}`,
             borderRadius: 1.5,
             padding: 1,
-          })}>
-          <Stack direction={"row"} justifyContent={"space-between"} alignItems={"center"}>
+          })}
+        >
+          <Stack
+            direction={"row"}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+          >
             <Typography>Request</Typography>
-            <IconButton edge={"end"} onClick={() => copyToClipboard(codeSnippet)}>
+            <IconButton
+              edge={"end"}
+              onClick={() => copyToClipboard(codeSnippet)}
+            >
               <CopyAllRounded />
             </IconButton>
           </Stack>
@@ -115,12 +159,13 @@ const RenderApiEndpoint = (props: APIEndpoint) => {
             border: `1px solid ${theme.palette.primary.main}`,
             borderRadius: 1.5,
             padding: 1,
-          })}>
+          })}
+        >
           <Typography>Response</Typography>
           <Divider />
           <pre>
             <code className="language-json">
-              {typeof props.response === "string" ? props.response : JSON.stringify(props.response, null, 2)}
+              {renderStringOrObject(props.response)}
             </code>
           </pre>
         </Box>
@@ -130,7 +175,22 @@ const RenderApiEndpoint = (props: APIEndpoint) => {
 };
 
 const getCodeSnippet = (props: APIEndpoint, language: "python" = "python") => {
+  if (props.method === "POST") {
+    return `import requests
+requests.post("${props.url}", json=${renderStringOrObject(props.request)})
+`;
+  }
+
+  if (props.method === "PUT") {
+    return `import requests
+requests.put("${props.url}", json=${renderStringOrObject(props.request)})
+`;
+  }
+
   return `import requests
-requests.get("${props.url}", params={"${props.input_query}":"required_value"})
+requests.get("${props.url}", ${renderStringOrObject(props.request)})
 `;
 };
+
+const renderStringOrObject = (data: object | string = "") =>
+  typeof data === "string" ? data : JSON.stringify(data, null, 2);
