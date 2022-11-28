4723c47406b56230ece62f65e5a7f5d21e260889
Jirka <jirka.borovec@seznam.cz>
2022-11-28 14:33:12 +0100

    if/else import

--- lightning_api_access/APIAccessFrontend.py
+++ lightning_api_access/APIAccessFrontend.py
@@ -2,17 +2,12 @@ import json
 import os
 from typing import Any, Dict, List
 
-try:
-    from lightning.app.frontend import StaticWebFrontend
-except ModuleNotFoundError:
-    # TODO properly check module availability
-    try:
-        from lightning_app.frontend import StaticWebFrontend
-    except ModuleNotFoundError:
+from lightning_utilities.core.imports import module_available
 
-        raise ModuleNotFoundError(
-            "Lightning is a required dependency for this component. Please run: pip install lightning"
-        )
+if module_available("lightning"):
+    from lightning.app.frontend import StaticWebFrontend
+else:
+    from lightning_app.frontend import StaticWebFrontend
 
 
 class APIAccessFrontend(StaticWebFrontend):
